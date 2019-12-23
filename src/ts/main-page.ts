c3c240a84e5659e0d9a458340a01f8ca55223d33
danil.khandys <danil.khandys@xpage.ru>
2019-12-23 15:43:51 +0500

    верстаю главную

--- src/ts/main-page.ts
+++ src/ts/main-page.ts
@@ -1,5 +1,6 @@
 import domReady from "./xpage/ready";
 import App from "./xpage/core";
+import { EventListener } from "./xpage/index";
 
 domReady(() => {    
     App.each(".ml-side", (el: HTMLElement, i: Number) => {
@@ -29,5 +30,52 @@ function setSideBlockBottomOffset(sideBlock: HTMLElement, blockIndex: Number){
 
 
 domReady(() => {
-    
+    new EventListener(".mr-side__burger .burger").add("click", () => {
+        if (!document.body.classList.contains("js__menu-opened")){
+            App.each(".mr-side__menu", (menuItem: HTMLElement) => {
+                menuItem.style.overflow = "hidden";
+
+                menuItem.style.width = "0";
+
+                menuItem.style.display = "block";
+
+                menuItem.style.width = `${menuItem.scrollWidth}px`;
+
+
+
+                new EventListener(menuItem).add("transitionend", (el: HTMLElement) => {
+                    if (el.style.height !== "0px")
+                        el.style.height = "auto";
+                    else
+                        el.style.display = "none";
+                }, {
+                    once: true
+                });
+            });
+        }else{
+            App.each(".mr-side__menu", (menuItem: HTMLElement) => {
+                menuItem.style.overflow = "hidden";
+
+                menuItem.style.width = `${menuItem.scrollWidth}px`;
+
+                getComputedStyle(menuItem, null).getPropertyValue("width");
+
+                menuItem.style.width = "0";
+
+                
+
+                new EventListener(menuItem).add("transitionend", (el: HTMLElement) => {
+                    if (el.style.width !== "0px")
+                        el.style.width = "auto";
+                    else
+                        el.style.display = "none";
+                }, {
+                    once: true
+                });
+            });
+        }
+
+        document.body.classList.toggle("js__menu-opened")
+
+    });
 });
