508d3b69cbc7dac206c1017beb2e01315acf9162
danil.khandys <danil.khandys@xpage.ru>
2019-12-26 15:55:43 +0500

    1232

--- src/js/common.js
+++ src/js/common.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-	const phoneInputs = document.querySelectorAll(".phone-input, .input--phone");
+	const phoneInputs = document.querySelectorAll(".phone-input, .input--phone, .input-phone");
 
 	if (!phoneInputs.length)
 		return;
@@ -40,16 +40,48 @@ document.addEventListener("DOMContentLoaded", () => {
 	;(async function(){
 		const IMask = await import("imask");
 
-		for (let i = 0; i < phoneInputs.length; i++){
-			const phoneInput = phoneInputs[i];
+		phoneInputsMask(phoneInputs, IMask);
 
-			new IMask.default(phoneInput, {
-				mask: '+{7}(000)000-00-00'
-			});
+		const config = {
+			childList: true,
+			subtree: true
+		};
+
+		// Функция обратного вызова при срабатывании мутации
+		const callback = function(mutationsList, observer) {
+			for (let mutation of mutationsList) {
+				if (mutation.addedNodes.length)
+					for (const node of mutation.addedNodes)
+						if (node instanceof HTMLElement)
+							phoneInputsMask(node.querySelectorAll(".phone-input, .input--phone"), IMask);
+			}
 		}
+
+		// Создаем экземпляр наблюдателя с указанной функцией обратного вызова
+		const observer = new MutationObserver(callback);
+
+		// Начинаем наблюдение за настроенными изменениями целевого элемента
+		observer.observe(document.body, config);
 		
 	})();
 });
+
+const phoneInputsMask = (inputs, IMask) => {
+	for (let i = 0; i < inputs.length; i++){
+		const phoneInput = inputs[i];
+
+		new IMask.default(phoneInput, {
+			mask: '+# (000) 000-00-00',
+			lazy: true,
+			placeholder: {
+				show: 'always'
+			},
+			definitions: {
+				"#": "7",
+			}
+		});
+	}
+};
 
 window.initFancybox = () => {
 	$(".fancybox").fancybox({
